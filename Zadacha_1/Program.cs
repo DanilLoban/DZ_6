99d50f65d936d18fb20f453bc6174f6412045c62
Danil <dan.loban@yandex.ru>
2022-12-20 22:12:16 +0300

    firstlyy

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите колличество чисел: ");
+int N = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < N; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int x = int.Parse(Console.ReadLine());
+    if (x >= 0)
+        count++;
+}
+Console.WriteLine($"Положительных чисел: {count}");
